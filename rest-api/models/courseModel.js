8d2f900a7468a2094b8f35bf854b6e082a98124b
garciacce23 <62458331+garciacce23@users.noreply.github.com>
2023-02-24 10:28:45 -0800

    REST-API has full CRUD functionality. MongoDB configuration is complete and has courses model set up as well as an ezge database witha catalogue collection to hold Fresno State courses. Environmental variables are also properly implemented.

---
+++ rest-api/models/courseModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+
+const courseSchema = new mongoose.Schema({
+
+    CRSE: {type: String, required: true},
+    CATALOG_NBR: {type: Number, required: true},
+    CRSE_CAREER: {type: String, required: true},
+    CRSE_DESCR: {type: String, required: true},
+    CRSE_ID: {type: Number, required: true},
+    CRSE_TITLE: {type: String, required: true},
+    CRSE_TYP_OFFR: {type: String, required: false, default: "NA"},
+    CRSE_UPDATE: {type: Date, required: false},
+    DEPT_ID: {type: Number, required: false},
+    GE_ATTRIBUTE: {type: String, required: false, default: "NA"},
+    SUBJECT_ID: {type: String, required: true},
+    UNITS_RANGE: {type: Number, required: true},
+    UNIT_REPEAT_LIMIT: {type: Number, required: false},
+    POS_ID: {type: Number, required: true}
+
+}, { collection: 'catalogue' });
+
+// courseSchema.index({ POS_ID: 1 }, { unique: true }); // Made redundant by init-mongo.js
+
+const CourseModel = mongoose.model('CourseModel', courseSchema);
+
+module.exports = CourseModel;
+
+/*
+"COMM-3": {
+        "CATALOG_NBR": "3",
+        "CRSE_CAREER": "UGRD",
+        "CRSE_DESCR": "Theories of human communication and their function in contemporary public settings; experiences designed to enhance fundamental communication skills -- research, organization, reasoning, listening, and problem solving -- through a series of oral presentations.  G.E. Foundation A1.",
+        "CRSE_ID": "002771",
+        "CRSE_TITLE": "Fundamentals of Public Communication",
+        "CRSE_TYP_OFFR": "Fall, Spring",
+        "CRSE_UPDATE": "2021-08-02",
+        "DEPT_ID": "686",
+        "GE_ATTRIBUTE": "A1",
+        "SUBJECT_ID": "COMM",
+        "UNITS_RANGE": "3",
+        "UNIT_REPEAT_LIMIT": "3"
+    },
+ */
